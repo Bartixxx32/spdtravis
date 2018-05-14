143d745b1104eefb3b1f805c5ab3765c5482327d
Bartixxx32 <mcbplay1@gmail.com>
2018-05-15 01:30:15 +0200

    Co

--- push.sh
+++ push.sh
@@ -6,7 +6,7 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout  master
+  git checkout -b master
   git add . test.txt
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
@@ -15,10 +15,9 @@ upload_files() {
   git remote -v
   git remote add origin
  https://${TEST}@github.com/Bartixxx32/spdtravis.git
-  git push --quiet --set-upstream origin/master
+  git push --quiet --set-upstream master
 }
 
 setup_git
 commit_website_files
 upload_files
-
