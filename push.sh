3a7355a31f38e556889f732bce4f41a9bb214fe5
Bartixxx32 <mcbplay1@gmail.com>
2018-05-15 01:16:36 +0200

    Co

--- push.sh
+++ push.sh
@@ -12,8 +12,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${SOMEVAR}@github.com/Bartixxx32/spdtravis.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin-pages https://${TEST}@github.com/Bartixxx32/spdtravis.git
+  git push --quiet --set-upstream origin-master master
 }
 
 setup_git
