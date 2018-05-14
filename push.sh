1b1982cb05518807846f9015bc08c5fc9120c437
Bartixxx32 <mcbplay1@gmail.com>
2018-05-15 01:57:54 +0200

    To

--- push.sh
+++ push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b master
+  git checkout master
   git add . test2.txt
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote -v
-  git remote add master https://${TEST}@github.com/Bartixxx32/spdtravis.git
+  git remote add master git remote add master https://b86d87e769348eb2c1ed3f508f726479ea767dcf@github.com/Bartixxx32/spdtravis.git
   git push --quiet --set-upstream master
 }
 
