bf9cec2da54187ff4ff0f170d4e6813fe2386024
Bartixxx32 <mcbplay1@gmail.com>
2018-05-15 00:29:58 +0200

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "mcbplay1@gmail.com"
+  git config --global user.name "Bartixxx32"
+}
+
+commit_website_files() {
+  git checkout -b master
+  git add . test.txt
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${SOMEVAR}@github.com/Bartixxx32/spdtravis.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+upload_files
