340b73896ca7f2a4915e54db4da90ee0c94a57d4
Bartixxx32 <mcbplay1@gmail.com>
2018-05-15 13:16:37 +0200

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "mcbplay1@gmail.com"
+  git config --global user.name "Bartixxx32"
+}
+
+commit_website_files() {
+  git checkout master
+  git add . test.txt
+  git commit --m testtravis
+}
+
+upload_files() {
+  git remote add master https://${secret}@github.com/Bartixxx32/spdtravis.git > /dev/null 2>&1
+  git push --quiet --set-upstream master
+}
+
+setup_git
+commit_website_files
+upload_files
